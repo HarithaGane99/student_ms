e02cc682cb616da025d6e98b311d5fb9dc64c17a
Haritha Ganegoda <haritha.99ganegoda@gmail.com>
2025-08-11 17:42:28 +0530

    auth & login

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,41 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import axios from 'axios';
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [user, setUser] = useState(null); 
+  const [isAuthenticated, setIsAuthenticated] = useState(!!token);
+
+  useEffect(() => {
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [token]);
+
+  const login = async (email, password) => {
+    const response = await axios.post('http://localhost:8070/api/auth/login', { email, password });
+    const { token } = response.data;
+    localStorage.setItem('token', token);
+    setToken(token);
+    window.location.reload(); 
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setToken(null);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
